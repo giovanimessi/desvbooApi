c047960ed82e9ba4e0e0fdbe9cc3e9ef09e23f09
Giovani <giovanimouradev@gmail.com>
2021-04-30 21:15:09 -0300

    Criacao das rota e dos controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FeedController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +18,34 @@ use Illuminate\Support\Facades\Route;
 Route::get("/ping",function(){
     return ['pong' =>true];
 });
+
+Route::get('/401',[AuthController::class, 'unauthorized'])->name('login');
+
+
+Route::post('/auth/login',[AuthController::class, 'login']);
+Route::post('/auth/logout',[AuthController::class, 'logout']);
+Route::post('/auth/refresh',[AuthController::class, 'resfresh']);
+
+Route::post('/user/criar',[UserController::class, 'create']);
+Route::put('/user',[UserController::class, 'update']);
+Route::post('/user/avatar',[UserController::class, 'UpdateAvatar']);
+Route::post('/user/cover',[UserController::class, 'UpdateCover']);
+
+Route::get('/feed',[FeedController::class, 'read']);
+Route::get('/user/feed',[FeedController::class, 'UserFeed']);
+Route::get('/user/{id}/feed',[FeedController::class, 'UserFeed']);
+
+Route::get('/user',[UserController::class, 'read']);
+Route::get('/user/{id}',[UserController::class, 'read']);
+
+Route::get('/feed',[FeedController::class, 'create']);
+
+Route::post('/post/{id}/like',[PostController::class, 'like']);
+Route::post('/post/{id}/comment',[PostController::class, 'comment']);
+
+Route::get('/search',[SearchController::class, 'search']);
+
+
+
+
+
